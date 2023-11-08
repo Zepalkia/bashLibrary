c509cd96a9f918927e250edf48b4ca52aafec6f0
Zepalkia <oritter1@hotmail.com>
2023-11-08 09:26:56 +0100

    Code improvements for WebSockets + new 'process' module

---
+++ lib/process.sh
@@ -0,0 +1,10 @@
+
+# Closes all the "non-default" file descriptors the CURRENT process has open or inherited from its parent
+function process_closeAllFileDescriptors() {
+  for fd in $(find /proc/$$/fd -type l -printf "%f\n"); do
+    # stdin (0), stdout (1), stderr(2) and the TTY connection (255 depending on the distribution) will NOT be closed
+    if [[ $fd -gt 2 ]] && [[ $fd -ne 255 ]]; then
+      eval "exec $fd>&-"
+    fi
+  done
+}
